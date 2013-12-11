3dd3dad455a9e74059f9774c551549075382206d
Lucio <Lucio@10.90.8.21>
2013-12-11 13:57:48 +0100

    primo

---
+++ BestAppMuseum/obj/local/armeabi/objs-debug/CloudReco/SampleMath.o.d
@@ -0,0 +1,105 @@
+obj/local/armeabi/objs-debug/CloudReco/SampleMath.o: jni/SampleMath.cpp \
+ jni/SampleMath.h jni/../../../build/include/QCAR/Tool.h \
+ jni/../../../build/include/QCAR/System.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ jni/../../../build/include/QCAR/Matrices.h \
+ jni/../../../build/include/QCAR/Vectors.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/math.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/limits.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/alloca.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/strings.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/memory.h
+
+jni/SampleMath.h:
+
+jni/../../../build/include/QCAR/Tool.h:
+
+jni/../../../build/include/QCAR/System.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+jni/../../../build/include/QCAR/Matrices.h:
+
+jni/../../../build/include/QCAR/Vectors.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/math.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/limits.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/strings.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/memory.h:
---
+++ BestAppMuseum/obj/local/armeabi/objs-debug/CloudReco/Transition3Dto2D.o.d
@@ -0,0 +1,108 @@
+obj/local/armeabi/objs-debug/CloudReco/Transition3Dto2D.o: \
+ jni/Transition3Dto2D.cpp jni/Transition3Dto2D.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h \
+ jni/../../../build/include/QCAR/QCAR.h \
+ jni/../../../build/include/QCAR/System.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ jni/../../../build/include/QCAR/Renderer.h \
+ jni/../../../build/include/QCAR/Matrices.h \
+ jni/../../../build/include/QCAR/Vectors.h \
+ jni/../../../build/include/QCAR/State.h \
+ jni/../../../build/include/QCAR/Frame.h \
+ jni/../../../build/include/QCAR/NonCopyable.h \
+ jni/../../../build/include/QCAR/Image.h jni/SampleUtils.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/log.h \
+ jni/Quad.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ jni/../../../build/include/QCAR/Tool.h
+
+jni/Transition3Dto2D.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
+
+jni/../../../build/include/QCAR/QCAR.h:
+
+jni/../../../build/include/QCAR/System.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+jni/../../../build/include/QCAR/Renderer.h:
+
+jni/../../../build/include/QCAR/Matrices.h:
+
+jni/../../../build/include/QCAR/Vectors.h:
+
+jni/../../../build/include/QCAR/State.h:
+
+jni/../../../build/include/QCAR/Frame.h:
+
+jni/../../../build/include/QCAR/NonCopyable.h:
+
+jni/../../../build/include/QCAR/Image.h:
+
+jni/SampleUtils.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+jni/Quad.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+C:/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+jni/../../../build/include/QCAR/Tool.h:
