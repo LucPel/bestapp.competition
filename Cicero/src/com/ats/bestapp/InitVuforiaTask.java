fb432a976a7d3bf81c0a4cc3d3fee68db35b3489
v.morra <v.morra@10.222.125.71>
2014-01-07 13:47:11 +0100

    dffvdf

---
+++ Cicero/src/com/ats/bestapp/InitVuforiaTask.java
@@ -0,0 +1,48 @@
+package com.ats.bestapp;
+
+import com.qualcomm.vuforia.Vuforia;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+public class InitVuforiaTask extends AsyncTask<Object, Integer, Boolean>{
+
+	 // Initialize with invalid value:
+    private int mProgressValue = -1;
+    
+    
+    protected Boolean doInBackground(Object... activities)
+    {
+        // Prevent the onDestroy() method to overlap with initialization:
+            Vuforia.setInitParameters((Activity)activities[0], Vuforia.GL_20);
+            
+            do
+            {
+                // Vuforia.init() blocks until an initialization step is
+                // complete, then it proceeds to the next step and reports
+                // progress in percents (0 ... 100%).
+                // If Vuforia.init() returns -1, it indicates an error.
+                // Initialization is done when progress has reached 100%.
+                mProgressValue = Vuforia.init();
+                
+                // Publish the progress value:
+                publishProgress(mProgressValue);
+                
+                // We check whether the task has been canceled in the
+                // meantime (by calling AsyncTask.cancel(true)).
+                // and bail out if it has, thus stopping this thread.
+                // This is necessary as the AsyncTask will run to completion
+                // regardless of the status of the component that
+                // started is.
+            } while (!isCancelled() && mProgressValue >= 0
+                && mProgressValue < 100);
+            
+            return (mProgressValue > 0);
+        }
+    
+    protected void onProgressUpdate(Integer... values)
+    {
+        // Do something with the progress value "values[0]", e.g. update
+        // splash screen, progress bar, etc.
+    }
+}
---
+++ Cicero/src/com/ats/bestapp/LoadTrackersTask.java
@@ -0,0 +1,19 @@
+package com.ats.bestapp;
+
+import android.os.AsyncTask;
+
+public class LoadTrackersTask extends AsyncTask<Object, Integer, Boolean>{
+
+	 protected Boolean doInBackground(Object... params)
+     {
+             VuforiaWrapper cw=(VuforiaWrapper)params[0];
+            // Load the tracker data set:
+			try {
+				cw.doLoadTrackersData();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return true;
+     }
+}
